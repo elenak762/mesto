03616bc929c3d7d3f15f3fc972f7bbf84c59a71b
elenak762 <elenak762@gmail.com>
2021-09-29 22:20:48 +0300

    поменяла адаптацию в popup

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,47 @@ function toggleModalWindow(){
 
 profileProjectLink.addEventListener('click',toggleModalWindow);
 modalWindowCloseBtn.addEventListener('click',toggleModalWindow);
+
+
+
+
+
+// Находим форму в DOM
+//let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+//let nameInput = document.querySelector('.popup__user-name');
+//let jobInput = document.querySelector('.popup__user-description');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+//function formSubmitHandler (evt) {
+    //evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    //document.getElementById("user-name-input").value;
+    //document.getElementById("user-description-input").value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    //let nameInput = document.querySelector('.popup__user-name');
+    //let jobInput = document.querySelector('.popup__user-description');
+    // Вставьте новые значения с помощью textContent
+    //textContent = "Имя";
+    //textContent ="Описание";
+//}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+//formElement.addEventListener('submit', formSubmitHandler);
+
+
+const form = modalWindow.querySelector('.popup__form');
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  console.log(e);
+})
+
+
+
+
